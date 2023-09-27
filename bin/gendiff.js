fea889998e3324251222063b3db11a6087054596
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-09-27 16:02:37 +0300

    step - 2: add arguments & action to see them, add option --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>', 'path to the files')
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+  });
 
 program.parse();
