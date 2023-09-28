3a490bf8a7eeb0530aaab06f0e9680856a173497
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-09-28 14:19:00 +0300

    fix: linter errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import { readFileSync } from 'node:fs';
 import { extname } from 'node:path';
 
-import { fileParser } from './parser.js';
+import fileParser from './parser.js';
 
 const readContent = (file) => readFileSync(file);
 const getFileExt = (file) => extname(file).slice(1);
 
-const getContent = (filePath_1, filePath_2) => {
-  const fileType_1 = getFileExt(filePath_1);
-  const fileType_2 = getFileExt(filePath_2);
-  const fileContent_1 = readContent(filePath_1);
-  const fileContent_2 = readContent(filePath_2);
-  const parseFileContent_1 = fileParser(fileContent_1, fileType_1);
-  const parseFileContent_2 = fileParser(fileContent_2, fileType_2);
+const getContent = (filePath1, filePath2) => {
+  const fileType1 = getFileExt(filePath1);
+  const fileType2 = getFileExt(filePath2);
+  const fileContent1 = readContent(filePath1);
+  const fileContent2 = readContent(filePath2);
+  const parseFileContent1 = fileParser(fileContent1, fileType1);
+  const parseFileContent2 = fileParser(fileContent2, fileType2);
 
-  return [parseFileContent_1, parseFileContent_2];
+  return [parseFileContent1, parseFileContent2];
 };
 
 export default getContent;
--- src/parser.js
+++ src/parser.js
@@ -2,13 +2,11 @@ const fileParser = (fileData, extention) => {
   switch (extention) {
     case 'json':
       return JSON.parse(fileData);
-      break;
     case 'yaml':
       return 'yaml';
-      break;
     default:
       throw Error(`This type is unknown - ${extention}!`);
   }
 };
 
-export { fileParser };
+export default fileParser;
