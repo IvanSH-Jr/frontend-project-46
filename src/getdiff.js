954b31136798d2be1c5ef62ca4dc8b316c62f6c1
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-10-04 14:25:55 +0300

    test

--- src/getdiff.js
+++ src/getdiff.js
@@ -2,42 +2,39 @@ import _ from 'lodash';
 
 const getDiff = (tree1, tree2) => {
   const sortedKeys = _.sortBy(_.union(Object.keys(tree1), Object.keys(tree2)));
-  const difference = {};
+  
   /*
 added (+) — ключ отсутствовал в первом объекте, но был добавлен во второй
 deleted (-)— ключ был в первом объекте, но отсутствует во втором
 changed (-)(+)— ключ присутствовал и в первом и во втором объектах, но значения отличаются
 unchanged — ключ присутствовал и в первом и во втором объектах с одинаковыми значениями
 */
-  sortedKeys.forEach((key) => {
+  const difference = sortedKeys.map((key) => {
     if (!Object.hasOwn(tree1, key)) {
-      difference[key] = 'added';
+      return {key, value: tree2[key], status: 'added'};
     } else if (!Object.hasOwn(tree2, key)) {
-      difference[key] = 'deleted';
+      return {key, value: tree1[key], status: 'deleted'};
     } else if (tree1[key] !== tree2[key]) {
-      difference[key] = 'changed';
+      return {key, value: tree1[key], newValue: tree2[key], status: 'changed'};
     } else {
-      difference[key] = 'unchanged';
+      return {key, value: tree1[key], status: 'unchanged'};
     }
   });
   console.log(difference);
   let result = '';
-  for (const [key, value] of Object.entries(difference)) {
-    if (value === 'added') {
-      result = `${result}\n+ ${key}: ${tree2[key]}`;
-    } else if (value === 'deleted') {
-      result = `${result}\n- ${key}: ${tree1[key]}`;
-    } else if (value === 'changed') {
-      result = `${result}\n- ${key}: ${tree1[key]}\n+ ${key}: ${tree2[key]}`;
+  difference.forEach(({key, value, newValue, status}) => {
+    if (status === 'added') {
+      result = `${result}\n+ ${key}: ${value}`;
+    } else if (status === 'deleted') {
+      result = `${result}\n- ${key}: ${value}`;
+    } else if (status === 'changed') {
+      result = `${result}\n- ${key}: ${value}\n+ ${key}: ${newValue}`;
     } else {
-      result = `${result}\n  ${key}: ${tree1[key]}`;
+      result = `${result}\n  ${key}: ${value}`;
     }
-  }
+  })
 
-  result = `{\n${result}\n}`;
-  console.log(result);
-
-  return result;
+  console.log(result)
 };
 
 export default getDiff;
