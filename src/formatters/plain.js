6cf86ee2a9a01c05a9d57dc6d61b367e63d017e5
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-10-23 13:03:35 +0300

    plain done

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,39 +1,40 @@
-import isObject from 'lodash';
-import isString from 'lodash';
+import _ from 'lodash';
 
 const complexValue = (val) => {
-  if (isObject(val)) {
+  if (_.isObject(val)) {
     return '[complex value]';
   }
 
-  if(isString(val)) return `'${val}'`;
+  if(_.isString(val)) return `'${val}'`;
 
   return `${val}`;
 }
 
 const plain = (value) => {
-  const iter = (curentVal) => {
-    const result = curentVal.flatMap(({key, oldValue, newValue, status, children,}) => {
-      console.log(key)
-      if (status === 'nested') {
-        return iter(children);
-      }
-      if (status === 'added') {
-        return `Property '${key}' was added with value: ${complexValue(newValue)}`;
-      }
-      if (status === 'deleted') {
-        return `Property '${key}' was removed`;
-      }
-      if (status === 'changed') {
-        return `Property '${key}' was updated. From ${complexValue(oldValue)} to ${complexValue(newValue)}`;
-      }
-      if (status === 'unchanged') {
-        return [];
-      }
-    });
+  const iter = (curentVal, ancestor) => {
+    const result = curentVal
+          .flatMap(({key, oldValue, newValue, status, children,}) => {
+            const pathToVal = `${ancestor}.${key}`;
+            if (status === 'nested') {
+              return iter(children, pathToVal);
+            }
+            if (status === 'added') {
+              return `Property '${pathToVal.slice(1)}' was added with value: ${complexValue(newValue)}`;
+            }
+            if (status === 'deleted') {
+              return `Property '${pathToVal.slice(1)}' was removed`;
+            }
+            if (status === 'changed') {
+              return `Property '${pathToVal.slice(1)}' was updated. From ${complexValue(oldValue)} to ${complexValue(newValue)}`;
+            }
+            if (status === 'unchanged') {
+              return [];
+            }
+          })
+          .join('\n');
     return result;
   };
-  return iter(value);
+  return iter(value, '');
 };
 
 export default plain;
