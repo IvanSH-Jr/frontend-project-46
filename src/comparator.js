022a2f5c751bcd3f93da61b791a70ea6541e3614
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-10-27 17:12:23 +0300

    remove fnc iter from comparator.js

--- src/comparator.js
+++ src/comparator.js
@@ -1,38 +1,33 @@
 import _ from 'lodash';
 
-export default (obj1, obj2) => {
-  const iter = (currentObj1, currentObj2) => {
-    if (_.isEmpty(currentObj1) && _.isEmpty(currentObj2)) return '{}';
-
-    const keysObj1 = Object.keys(currentObj1);
-    const keysObj2 = Object.keys(currentObj2);
-    const unionKeys = _.union(keysObj1, keysObj2);
-    const sortedKeys = _.sortBy(unionKeys);
-
-    const statusesOfValues = sortedKeys.flatMap((key) => {
-      if (_.isObject(currentObj1[key]) && _.isObject(currentObj2[key])) {
-        return { key, status: 'nested', children: iter(currentObj1[key], currentObj2[key]) };
-      }
-      if (!Object.hasOwn(currentObj1, key)) {
-        return { key, newValue: currentObj2[key], status: 'added' };
-      }
-      if (!Object.hasOwn(currentObj2, key)) {
-        return { key, oldValue: currentObj1[key], status: 'deleted' };
-      }
-      if (
-        currentObj1[key] === currentObj2[key]
-      ) {
-        return {
-          key, oldValue: currentObj1[key], status: 'unchanged',
-        };
-      }
-
+const compareTrees = (obj1, obj2) => {
+  if (_.isEmpty(obj1) && _.isEmpty(obj2)) return '{}';
+  const keysObj1 = Object.keys(obj1);
+  const keysObj2 = Object.keys(obj2);
+  const unionKeys = _.union(keysObj1, keysObj2);
+  const sortedKeys = _.sortBy(unionKeys);
+  const result = sortedKeys.flatMap((key) => {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, status: 'nested', children: compareTrees(obj1[key], obj2[key]) };
+    }
+    if (!Object.hasOwn(obj1, key)) {
+      return { key, newValue: obj2[key], status: 'added' };
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      return { key, oldValue: obj1[key], status: 'deleted' };
+    }
+    if (
+      obj1[key] === obj2[key]
+    ) {
       return {
-        key, oldValue: currentObj1[key], newValue: currentObj2[key], status: 'changed',
+        key, oldValue: obj1[key], status: 'unchanged',
       };
-    });
-    return statusesOfValues;
-  };
-
-  return iter(obj1, obj2);
+    }
+    return {
+      key, oldValue: obj1[key], newValue: obj2[key], status: 'changed',
+    };
+  });
+  return result;
 };
+
+export default compareTrees;
