2a362e3cbbb8f40e88a7c457015e9f4c3779fc2b
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-10-30 18:29:18 +0300

    fix file name

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,12 +12,12 @@ const makePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
 const readFile = (pathToFile) => fs.readFileSync(makePath(pathToFile), 'utf8');
 
 test.each([
-  ['filejson1.json', 'filejson2.json', 'stylish', 'result/result1.txt'],
-  ['filejson1.json', 'filejson2.json', 'plain', 'result/result2.txt'],
-  ['filejson1.json', 'filejson2.json', 'json', 'result/result3.txt'],
-  ['fileyml1.yml', 'fileyml2.yml', 'stylish', 'result/result1.txt'],
-  ['fileyml1.yml', 'fileyml2.yml', 'plain', 'result/result2.txt'],
-  ['fileyml1.yml', 'fileyml2.yml', 'json', 'result/result3.txt'],
+  ['file1.json', 'file2.json', 'stylish', 'result/result1.txt'],
+  ['file1.json', 'file2.json', 'plain', 'result/result2.txt'],
+  ['file1.json', 'file2.json', 'json', 'result/result3.txt'],
+  ['file1.yml', 'file2.yml', 'stylish', 'result/result1.txt'],
+  ['file1.yml', 'file2.yml', 'plain', 'result/result2.txt'],
+  ['file1.yml', 'file2.yml', 'json', 'result/result3.txt'],
 ])('Compare %p and %p, result in %s format', (file1, file2, format, result) => {
   expect(genDiff(getFixturePath(file1), getFixturePath(file2), format).replace(/\s/g, ''))
     .toEqual(readFile(getFixturePath(result)).replace(/\s/g, ''));
