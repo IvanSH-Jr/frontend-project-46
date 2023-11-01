a456eaf2dc294fdfd05718e1aa41682131d19bb6
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-11-01 11:47:15 +0300

    fix mentor comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
 import fileParser from './parser.js';
-import { readFile, getFileExt } from './fileReader.js';
 import compareTrees from './comparator.js';
 import formatter from './formatters/index.js';
 
+const getFileExt = (filePath) => path.extname(filePath).slice(0);
+const makePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(makePath(filePath), 'utf8');
+
+const getParsedData = (filePath) => {
+  const fileContent = readFile(filePath);
+  const fileExt = getFileExt(filePath);
+  const parsedData = fileParser(fileContent, fileExt);
+  return parsedData;
+};
+
 const genDiff = (filePath1, filePath2, format = 'stylish') => {
-  const fileContent1 = readFile(filePath1);
-  const fileContent2 = readFile(filePath2);
-  const fileExt1 = getFileExt(filePath1);
-  const fileExt2 = getFileExt(filePath2);
-  const parsedFile1 = fileParser(fileContent1, fileExt1);
-  const parsedFile2 = fileParser(fileContent2, fileExt2);
+  const [parsedFile1, parsedFile2] = [getParsedData(filePath1), getParsedData(filePath2)];
   const treeOfDiff = compareTrees(parsedFile1, parsedFile2);
   const result = formatter(treeOfDiff, format);
   return result;
--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,16 @@
 import yaml from 'js-yaml';
 
-const fileParser = (file, ext) => {
-  if (ext === '.json') return JSON.parse(file);
-
-  if (ext === '.yaml' || ext === '.yml') {
-    return yaml.load(file);
+const fileParser = (data, ext) => {
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+      return yaml.load(data);
+    case '.yml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown extention of file: '${ext}'!`);
   }
-
-  throw Error(`This type is unknown - ${ext}!`);
 };
 
 export default fileParser;
